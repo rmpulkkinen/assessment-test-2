1d82761398ff8958506d207b4a501cef05d0b63e
Risto-Matti Pulkkinen <rm.pulkkinen@gmail.com>
2022-10-07 09:56:24 +0300

    whoops commit everything

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,21 @@
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+function calculateTotal(items) {
+  return items.reduce((a, b) => {
+    return a + b.price;
+  }, 0);
 }
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+function calculateAvg(items) {
+  return (
+    items.reduce((a, b) => {
+      return a + b.price;
+    }, 0) / items.length
+  );
 }
 
 export { calculateTotal, calculateAvg };
